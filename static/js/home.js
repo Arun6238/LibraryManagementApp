7130ac670311905c602f78d8f2f0a4c5df71799d
Arun Suresh <arunsuresh241@gmail.com>
2023-02-22 15:26:41 +0530

    restyled navbar with plane css modified: static/css/navbar.css, static/js/home.js, templates/navbar.html

--- static/js/home.js
+++ static/js/home.js
@@ -1,5 +1,10 @@
 
 function profileDropdown(){
     let submenu = document.getElementById("profileMenu")
-    submenu.classList.toggle("open-menu")
+    submenu.classList.add("open-menu")
+}
+
+function profileClose(){
+    let submenu = document.getElementById("profileMenu")
+    submenu.classList.remove("open-menu")
 }
